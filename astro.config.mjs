e15b9f00168aaa382b90bda390a06b097edfbaa6
DessieOwO <dessie@dessieowo.com>
2026-05-26 18:46:11 -0400

    Add blog
    Weeeryefgfuw :P

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig,passthroughImageService } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dessieowo.com',
   output: 'server',
   adapter: cloudflare(),
+
   image:  {
     service: passthroughImageService()
-  }
+  },
+
+  integrations: [mdx()]
 });
